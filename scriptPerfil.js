b23507fd87161f1bb51bce1e2ad3545c72f1424a
Pedro Henrique <pedrohenriquemartinscorrea@gmail.com>
2025-02-07 01:09:03 -0300

    projeto inicial

---
+++ scriptPerfil.js
@@ -0,0 +1,37 @@
+function showTime() {
+	document.getElementById('currentTime').innerHTML = new Date().toUTCString();
+}
+showTime();
+setInterval(function () {
+	showTime();
+}, 1000);
+
+function resumo() {
+  document.getElementById('lista_container_resumo').style.display = 'flex';
+  document.getElementById('lista_itens-resumo').style.display = 'block'
+}
+
+function habilidades() {
+    document.getElementById('lista_container_habilidades').style.display = 'flex';
+  document.getElementById('lista__itens-habilidades').style.display = 'block';
+}
+
+function experiencias() {
+    document.getElementById('lista_container_experiencias').style.display = 'flex';
+  document.getElementById('lista__itens-experiencias').style.display = 'block';
+}
+
+function formacao() {
+    document.getElementById('lista_container_formacao').style.display = 'flex';
+  document.getElementById('lista__itens-formacao').style.display = 'block';
+}
+
+function cursos() {
+    document.getElementById('lista_container_cursos').style.display = 'flex';
+  document.getElementById('lista__itens-cursos').style.display = 'block';
+}
+
+function certificados() {
+    document.getElementById('lista_container_certificados').style.display = 'flex';
+  document.getElementById('lista__itens-certificados').style.display = 'block';
+}
